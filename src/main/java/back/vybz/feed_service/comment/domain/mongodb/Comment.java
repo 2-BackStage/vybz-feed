356b0283c5b748b694cb6dfc096de3784844ef2b
jin-sung-oh <wlsqoren@naver.com>
2025-05-21 17:09:40 +0900

    feat:댓글 entity 생성

--- src/main/java/back/vybz/feed_service/comment/domain/mongodb/Comment.java
+++ src/main/java/back/vybz/feed_service/comment/domain/mongodb/Comment.java
@@ -1,4 +1,4 @@
-package back.vybz.feed_service.like.domain.mongodb;
+package back.vybz.feed_service.comment.domain.mongodb;
 
 import back.vybz.feed_service.busker.domain.mongodb.FeedType;
 import lombok.Builder;
@@ -6,9 +6,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.time.Instant;
@@ -16,7 +16,7 @@ import java.time.Instant;
 @Getter
 @NoArgsConstructor
 @Document
-public class FeedLike {
+public class Comment {
 
     @Id
     private ObjectId id;
@@ -29,6 +29,10 @@ public class FeedLike {
     @Field(name = "feed_type")
     private FeedType feedType;
 
+    //댓글작성자 uuid
+    @Field(name = " writer_uuid")
+    private String writerUuid;
+
     // 피드 작성자 uuid
     @Field(name = "user_uuid")
     private String userUuid;
@@ -36,6 +40,10 @@ public class FeedLike {
     //버스커 uuid
     @Field(name = "busker_uuid")
     private String buskerUuid;
+
+    // 댓글 내용
+    @Field(name = "comment")
+    private String comment;
 
     @CreatedDate
     @Field(name = "created_at")
@@ -46,19 +54,20 @@ public class FeedLike {
     private Instant updatedAt;
 
     @Builder
-    public FeedLike(ObjectId id,
-                    ObjectId feedId,
-                    FeedType feedType,
-                    String userUuid,
-                    String buskerUuid,
-                    Instant createdAt,
-                    Instant updatedAt) {
+    public Comment(ObjectId id,
+                   ObjectId feedId,
+                   FeedType feedType,
+                   String writerUuid,
+                   String userUuid,
+                   String buskerUuid,
+                   String comment) {
         this.id = id;
         this.feedId = feedId;
         this.feedType = feedType;
+        this.writerUuid = writerUuid;
         this.userUuid = userUuid;
         this.buskerUuid = buskerUuid;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
+        this.comment = comment;
     }
+
 }
--- src/main/java/back/vybz/feed_service/like/domain/mongodb/CommentLike.java
+++ src/main/java/back/vybz/feed_service/like/domain/mongodb/CommentLike.java
@@ -1,12 +1,17 @@
 package back.vybz.feed_service.like.domain.mongodb;
 
 import back.vybz.feed_service.busker.domain.mongodb.FeedType;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.Instant;
 
 @Getter
 @NoArgsConstructor
@@ -40,4 +45,38 @@ public class CommentLike {
     @Field(name = "busker_uuid")
     private String buskerUuid;
 
+    //대댓글 id
+    @Field(name = " parent_comment_id")
+    private ObjectId parentCommentId;
+
+    @CreatedDate
+    @Field(name = "created_at")
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Field(name = "updated_at")
+    private Instant updatedAt;
+
+    @Builder
+    public CommentLike(ObjectId id,
+                       ObjectId feedId,
+                       FeedType feedType,
+                       ObjectId commentId,
+                       String writerUuid,
+                       String userUuid,
+                       String buskerUuid,
+                       ObjectId parentCommentId,
+                       Instant createdAt,
+                       Instant updatedAt) {
+        this.id = id;
+        this.feedId = feedId;
+        this.feedType = feedType;
+        this.commentId = commentId;
+        this.writerUuid = writerUuid;
+        this.userUuid = userUuid;
+        this.buskerUuid = buskerUuid;
+        this.parentCommentId = parentCommentId;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
 }
