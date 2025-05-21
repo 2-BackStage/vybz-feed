a7994c48542c9b2b8441213de05a3305a244a7e5
jijihorang <jiho7213@gmail.com>
2025-05-21 16:42:18 +0900

    feat: user feed, location, feedFile document 설계

---
+++ src/main/java/back/vybz/feed_service/user/domain/mongodb/FeedFile.java
@@ -0,0 +1,13 @@
+package back.vybz.feed_service.user.domain.mongodb;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class FeedFile {
+
+    private String name;
+
+    private String url;
+}
---
+++ src/main/java/back/vybz/feed_service/user/domain/mongodb/UserFeed.java
@@ -0,0 +1,73 @@
+package back.vybz.feed_service.user.domain.mongodb;
+
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.Instant;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Document("user_feed")
+public class UserFeed {
+
+    @Id
+    private ObjectId id;
+
+    @Field(name = "user_uuid")
+    private String userUuid;
+
+    @Field(name = "content")
+    private String content;
+
+    @Field(name = "humanTag")
+    private String humanTag;
+
+    @Field(name = "hasTag")
+    private String hasTag;
+
+    @Field(name = "file_list")
+    private List<FeedFile> fileList;
+
+    @Field(name = "location")
+    private Location location;
+
+    @Field(name = "comment_count")
+    private Integer commentCount = 0;
+
+    @Field(name = "like_count")
+    private Integer likeCount = 0;
+
+    @CreatedDate
+    @Field(name = "created_at")
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Field(name = "updated_at")
+    private Instant updatedAt;
+
+    @Builder
+    public UserFeed(ObjectId id, String userUuid, String content, String humanTag, String hasTag,
+                    List<FeedFile> fileList, Location location,
+                    Integer commentCount, Integer likeCount,
+                    Instant createdAt, Instant updatedAt) {
+        this.id = id;
+        this.userUuid = userUuid;
+        this.content = content;
+        this.humanTag = humanTag;
+        this.hasTag = hasTag;
+        this.fileList = fileList;
+        this.location = location;
+        this.commentCount = commentCount;
+        this.likeCount = likeCount;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+}
