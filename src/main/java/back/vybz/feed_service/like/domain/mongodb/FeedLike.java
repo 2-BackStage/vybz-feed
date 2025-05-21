de98721c5ceeb4bb59f2668979bcf9d550c8bef2
jin-sung-oh <wlsqoren@naver.com>
2025-05-21 16:57:04 +0900

    feat:피드 좋아요 entity 생성

---
+++ src/main/java/back/vybz/feed_service/like/domain/mongodb/FeedLike.java
@@ -0,0 +1,34 @@
+package back.vybz.feed_service.like.domain.mongodb;
+
+import back.vybz.feed_service.busker.domain.mongodb.FeedType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Getter
+@NoArgsConstructor
+@Document
+public class FeedLike {
+
+    @Id
+    private ObjectId id;
+
+    // 피드 id
+    @Field(name = "feed_id")
+    private ObjectId feedId;
+
+    // 피드 타입
+    @Field(name = "feed_type")
+    private FeedType feedType;
+
+    // 피드 작성자 uuid
+    @Field(name = "user_uuid")
+    private String userUuid;
+
+    //버스커 uuid
+    @Field(name = "busker_uuid")
+    private String buskerUuid;
+}
