12366793b0fe454c8bc0df5f3cca8ee046426dc2
jin-sung-oh <wlsqoren@naver.com>
2025-06-04 14:19:09 +0900

    refactor: controller수정

--- src/main/java/back/vybz/feed_service/like/infrastructure/CommentLikeRepositoryCustom.java
+++
@@ -1,5 +0,0 @@
-//package back.vybz.feed_service.like.infrastructure;
-//
-//public interface CommentLikeRepositoryCustom {
-//    int incLikeCount(String commentId, int delta, String collectionName);
-//}
