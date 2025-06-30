3dfa13025e4f2ac25ac468614b8cf6f9a1967a6a
jin-sung-oh <wlsqoren@naver.com>
2025-06-30 10:40:46 +0900

    fix:entity수정

--- src/main/java/back/vybz/feed_service/feed/domain/mongodb/Feed.java
+++ src/main/java/back/vybz/feed_service/feed/domain/mongodb/Feed.java
@@ -21,39 +21,51 @@ public class Feed {
     private String id;
 
     //작성자 uuid
+    @Field(name = "writer_uuid")
     private String writerUuid;
 
     // 작성자 타입
+    @Field(name = "writer_type")
     private WriterType writerType;
 
     //공지 제목
+    @Field(name = "title")
     private String title;
 
     // 시작 일시
+    @Field(name = "started_at")
     private Instant startedAt;
 
     // 종료 일시
+    @Field(name = "ended_at")
     private Instant endedAt;
 
     // 피드 내용
+    @Field(name = "content")
     private String content;
 
     // 사람 태그
+    @Field(name = "human_tag")
     private List<TaggedHuman> humanTag;
 
     // 해시 태그
+    @Field(name = "hash_tag")
     private List<String> hashTag;
 
     // 피드 타입
+    @Field(name = "feed_type")
     private FeedType feedType;
 
     // 파일 리스트
+    @Field(name = "file_list")
     private List<FeedFile> fileList;
 
     // 위치
+    @Field(name = "location")
     private String location;
 
     // 구독자 전용 여부
+    @Field(name = "membership")
     private Boolean membership;
 
     @CreatedDate
